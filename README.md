7393eb4e6fd71845f7287c417dbd6e4d488dde7e
Dika yonanda putra <yonandaputra05@gmail.com>
2025-03-21 14:27:50 +0700

    add image + update readme

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 A new Flutter project.
 ![Screenshoot Hello_World](images/01.png)
+
+### soal 1
+![Screenshoot Hello_World](images/p1s1.gif)
+aplikasi bisa menambahkan teks dengan menekan tombol +, bisa di centang, dan bisa di scroll jika item tidak muat di layar
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:master_plan/main.dart';
+import 'package:master_plan/views/plan_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PlanScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
